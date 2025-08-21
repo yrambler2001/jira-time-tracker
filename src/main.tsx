f1f37b852d1f9a2e15ed0c80713cca27aecfef23
Yurii Synyshyn <yrambler2001@gmail.com>
2025-08-21 22:20:55 +0200

    build & redirects

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './chart.css'
-import './App.css'
+// import './App.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
